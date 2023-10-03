54ff85fad7f58b99d7ecf4581f9387cc9e7a8cc3
DG <diegogiron67@yahoo.com>
2023-10-02 23:31:10 -0700

    Update views.py

--- django-test/mysite/polls/views.py
+++ django-test/mysite/polls/views.py
@@ -1,8 +1,14 @@
+
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+import requests
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def url_rtrn(request):
+    url = 'https://youtube.com'
+    response = requests.get(url)
+    return HttpResponse(url)
